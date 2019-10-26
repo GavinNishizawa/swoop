68e9512f30b31adee0143b7afbd03ce91dd57b4f
Gavin Nishizawa <ghn6069@rit.edu>
2019-10-26 13:03:17 -0400

    Test component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { make as TestComponent } from './TestComponent.bs';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <canvas/>
+        <TestComponent />
       </header>
     </div>
   );
