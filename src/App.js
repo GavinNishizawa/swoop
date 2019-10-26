7c806976609a46f27c5f755383b17e60e141af6c
pikachungg <34949943+pikachungg@users.noreply.github.com>
2019-10-26 15:02:25 -0400

    css and logo
    added css and logo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import TestComponent from './TestComponent.bs';
 import Draw from './Draw.js';
+import logo from './assests/swoop.png'
 
 class App extends React.Component {
   componentDidMount(){
@@ -12,6 +13,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
+        <img className="App-logo" src={logo} alt="swoop-logo"/>
           <canvas id='canvas' width="1200" height="600"/>
           <TestComponent />
         </header>
