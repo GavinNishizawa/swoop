0d41067738e3b518b02818fa4572a444f2290111
Gavin Nishizawa <ghn6069@rit.edu>
2019-10-27 12:18:03 -0400

    Move build to docs

---
+++ docs/precache-manifest.aea396092ab8f2b9fe5826eed167767c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37e4d520fbf8b61fc57a3804c667139",
+    "url": "/index.html"
+  },
+  {
+    "revision": "763683f00c1e2a7a6d75",
+    "url": "/static/css/main.62c0bd08.chunk.css"
+  },
+  {
+    "revision": "0da9deeb3b908203dbe0",
+    "url": "/static/js/2.e177d01d.chunk.js"
+  },
+  {
+    "revision": "763683f00c1e2a7a6d75",
+    "url": "/static/js/main.355fff30.chunk.js"
+  },
+  {
+    "revision": "6946578d41535329222e",
+    "url": "/static/js/runtime-main.72efb767.js"
+  },
+  {
+    "revision": "a31528deb4d248974ea9817628c94e98",
+    "url": "/static/media/swoop.a31528de.png"
+  }
+]);
